e0fdb51a54a65dcd6ae251ba56666056514e152c
YanghaoWu <939806382@qq.com>
2017-05-22 21:08:57 +0800

    Create Multiline.py

---
+++ Python/Day-02/Multiline.py
@@ -0,0 +1,10 @@
+#！/usr/bin/python
+#多行语句方式一
+total1 = "item_one" + \
+        "item_two" + \
+        "item_three"
+#多行语句方式二
+total2 = ['item_one', 'item_two', 'item_three',
+        'item_four', 'item_five']
+print (total1)
+print (total2)
