4b695ad189d4b1ab4f727f93bb5d3e4108f8d76f
WUYANGHAO <939806382@qq.com>
2017-12-22 18:17:35 +0800

    Create chrome.bat

---
+++ chrome.bat
@@ -0,0 +1,24 @@
+@echo off
+set Program=%cd%\Chrome63\chrome.exe
+set LnkName=Chrome_WYH
+set WorkDir=
+
+if not defined WorkDir call:GetWorkDir "%Program%"
+(echo Set WshShell=CreateObject("WScript.Shell"^)
+echo strDesKtop=WshShell.SpecialFolders("DesKtop"^)
+echo Set oShellLink=WshShell.CreateShortcut(strDesKtop^&"\%LnkName%.lnk"^)
+echo oShellLink.TargetPath="%Program%"
+echo oShelllink.Arguments="--user-data-dir=UserData"
+echo oShellLink.WorkingDirectory="%WorkDir%"
+echo oShellLink.WindowStyle=1
+echo oShellLink.Description="%Desc%"
+echo oShellLink.Save)>makelnk.vbs
+echo 桌面快捷方式创建成功！
+makelnk.vbs
+del /f /q makelnk.vbs
+exit
+goto :eof
+:GetWorkDir
+set WorkDir=%~dp1
+set WorkDir=%WorkDir:~,-1%
+goto :eof
